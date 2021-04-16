3db6ab407d984619347ae09cca6fa81c57f60744
Titouan Galopin <galopintitouan@gmail.com>
2021-04-16 10:52:04 +0200

    Add about page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -18,4 +18,13 @@ class HomeController extends AbstractController
             'books' => $repository->getBooks(),
         ]);
     }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about(): Response
+    {
+        dump('ok');
+        exit;
+    }
 }
